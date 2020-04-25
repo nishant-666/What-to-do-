913b65aea6ee187d897022cb850087df0a0c83a8
nishant666 <nishants440@gmail.com>
2020-04-25 14:36:04 +0530

    Updates

---
+++ precache-manifest.3ef1df95195a929ab60089b467744af8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e146f34f588f510971afbd22d6792814",
+    "url": "/What-to-do-/index.html"
+  },
+  {
+    "revision": "6b780b33f8dccd41369d",
+    "url": "/What-to-do-/static/css/main.8a614e5a.chunk.css"
+  },
+  {
+    "revision": "c2757e73f306b01bb51c",
+    "url": "/What-to-do-/static/js/2.ce11f024.chunk.js"
+  },
+  {
+    "revision": "6b780b33f8dccd41369d",
+    "url": "/What-to-do-/static/js/main.67eaf8d7.chunk.js"
+  },
+  {
+    "revision": "2cb3c86b16c1ecf64551",
+    "url": "/What-to-do-/static/js/runtime-main.05d272ae.js"
+  }
+]);
